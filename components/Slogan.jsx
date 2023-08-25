c39dea10bdfb5a30cf31855c10bc5d1ca8414f92
Kamga-Roland <106543063+Kamga-Roland@users.noreply.github.com>
2023-08-25 13:16:48 +0100

    Update Slogan.jsx

--- components/Slogan.jsx
+++ components/Slogan.jsx
@@ -12,28 +12,33 @@ const Slogan = () => (
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+      className={`${styles.innerWidth} mx-auto`}
     >
-      <motion.div
-        variants={fadeIn('right', 'linear', 0.2, 1)}
-        className="flex-[0.95] flex justify-center flex-col"
-      >
-        <TitleText title={<>Fertiliseur de <span className="text-[#6a79ff]">l'entrepreneuriat africain</span>, pour la diaspora.</>} />
-        <div className="mt-[20px] text-justify text-[18px]">
-          Account's incarne une plateforme innovante et engagée. Notre objectif est de fertiliser les aspirations entrepreneuriales des membres de la diaspora africaine en facilitant leurs investissements et en renforçant leur impact dans leur pays d'origine.
-        </div>
-      </motion.div>
+      <div className="text-center mb-10">
+        <TitleText title={<>Notre <span className="text-[#6a79ff]">Leitmotiv</span></>} />
+      </div>
+      <div className=" mx-auto flex lg:flex-row flex-col gap-8">
+        <motion.div
+          variants={fadeIn('right', 'linear', 0.2, 1)}
+          className="flex-[0.95] flex justify-center flex-col"
+        >
+          <TitleText title={<>Fertiliseur de <span className="text-[#6a79ff]">l'entrepreneuriat africain</span>, pour la diaspora.</>} />
+          <div className="mt-[20px] text-justify text-[18px]">
+            Account's incarne une plateforme innovante et engagée. Notre objectif est de fertiliser les aspirations entrepreneuriales des membres de la diaspora africaine en facilitant leurs investissements et en renforçant leur impact dans leur pays d'origine.
+          </div>
+        </motion.div>
 
-      <motion.div
-        variants={fadeIn('right', 'linear', 0.2, 1)}
-        className="flex-[0.95] flex justify-center flex-col"
-      >
-        <img
-          src="/fertiliseur.png"
-          alt="get-started"
-          className="w-[80%] h-auto rounded-3xl object-contain "
-        />
-      </motion.div>
+        <motion.div
+          variants={fadeIn('right', 'linear', 0.2, 1)}
+          className="flex-[0.95] flex justify-center flex-col"
+        >
+          <img
+            src="/fertiliseur.png"
+            alt="get-started"
+            className="w-[80%] h-auto rounded-3xl object-contain "
+          />
+        </motion.div>
+      </div>
     </motion.div>
   </section>
 );
