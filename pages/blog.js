44f8033023459c6ca3fa4c09cf7f6ecf8b432bab
Kamga-Roland <106543063+Kamga-Roland@users.noreply.github.com>
2023-08-26 23:12:08 +0100

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,10 +1,19 @@
 import { Navbar } from '../components';
-import BlogContent from '../sections/BlogContent';
+import styles from '../styles';
 
 const Blog = () => (
   <div>
     <Navbar />
-    <BlogContent />
+    <section className={`${styles.paddings} relative z-10`}>
+      <div className={`${styles.xPaddings}`}>
+        <div className="text-center mt-20 font-bold">
+          Aucune publication disponible.
+        </div>
+        <div className={`${styles.paddings}`}>
+          <img src="/blog.svg" className="mt-5 mx-auto px-6 items-center justify-center w-25 h-25 " />
+        </div>
+      </div>
+    </section>
   </div>
 );
 
