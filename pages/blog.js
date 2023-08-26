d99dd4d3d6cac3047b4afab192414948964e7221
Kamga-Roland <106543063+Kamga-Roland@users.noreply.github.com>
2023-08-26 23:26:42 +0100

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -6,11 +6,11 @@ const Blog = () => (
     <Navbar />
     <section className={`${styles.paddings} relative z-10`}>
       <div className={`${styles.xPaddings}`}>
-        <div className="text-center mt-20 font-bold">
+        <div className="text-center mt-20 text-[#6a79ff] font-extrabold">
           Aucune publication disponible.
         </div>
         <div className={`${styles.paddings}`}>
-          <img src="/blog.svg" className="mt-5 mx-auto px-6 items-center justify-center w-25 h-25 " />
+          <img src="/blog.svg" className="mt-2 mx-auto px-6 items-center justify-center w-25 h-25 " />
         </div>
       </div>
     </section>
