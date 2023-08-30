7a19bf40f0173edb2949d80d311eb86e9d83b6e2
Kamga-Roland <106543063+Kamga-Roland@users.noreply.github.com>
2023-08-30 12:10:22 +0100

    Update World.jsx

--- sections/World.jsx
+++ sections/World.jsx
@@ -16,29 +16,29 @@ const World = () => (
     >
       <TitleText
         title={
-          <>De <span className="text-[#6a79ff]">nombreuses entités</span> à travers le monde nous font confiance.</>
+          <>De <span className="text-[#6a79ff]">nombreuses entités</span> à travers le monde nous Font Confiance.</>
         }
         textStyles="text-center"
       />
 
       <motion.div
         variants={fadeIn('up', 'tween', 0.3, 1)}
-        className="relative mt-[120px] flex w-full h-[200px]"
+        className="relative md:mt-[120px] flex w-full md:h-[200px]"
       >
-        <ul className="flex mx-auto space-x-3">
-          <li className="w-[20%] mt-5 mr-[8%] h-[50%]">
+        <ul className="md:flex md:mx-auto md:space-x-3">
+          <li className="w-20 ml-20 md:w-[20%] mt-5 md:mr-[8%] h-40 md:h-[50%]">
             <img src="/cinaf.png" className=" w-full h-full object-contain" />
           </li>
-          <li className="w-[70%] h-[70%]">
+          <li className="md:w-[70%] h-40 md:h-[70%]">
             <img src="/AFROPN.png" className=" w-full h-full object-contain" />
           </li>
-          <li className="w-[70%] h-[70%]">
+          <li className="md:w-[70%] h-40 md:h-[70%]">
             <img src="/50AFC.png" className=" w-full h-full object-contain" />
           </li>
-          <li className="w-[70%] h-[70%]">
+          <li className="md:w-[70%] md:h-[70%]">
             <img src="/MbC.png" className=" w-full h-full object-contain" />
           </li>
-          <li className="w-[25%] mt-4 h-[50%]">
+          <li className=" md:w-[25%] mt-4 h-40 md:h-[50%]">
             <img src="/NADOSSO.png" className=" w-full h-full object-contain" />
           </li>
         </ul>
